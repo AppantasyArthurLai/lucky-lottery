838ff6d6481c1724fa5bfac1451c30d49bf95d2e
孙荣忠 <rzsun@86links.com>
2019-01-10 13:21:50 +0800

    #

--- config.js
+++ config.js
@@ -5,17 +5,17 @@ var config =  {
     //背景图片
     background:'images/bg-1.png',
 
-    title: '公司年会',
-    titleFontSize: '32px',
+    title: '2019 集商网络科技（上海）公司年会',
+    titleFontSize: '55px',
     titleColor:'#fdd312',
 
     subTitle: '幸运大抽奖',
     subTitleFontSize: '50px',
     subTitleColor:'#fdd312',
     
-    rewardTitleFontSize:'32px',
+    rewardTitleFontSize:'80px',
     rewardTitleColor:'#fdd312',
-    rewardAreaTop: "0", //10%
+    rewardAreaTop: "20%", //10%
     rewardWelcomeMessage: "激动人心的时刻即将开始！",
 
     onceEndMessage:"本轮抽奖结束，更多精彩在下一轮！",
@@ -25,7 +25,7 @@ var config =  {
     //本系统手动停止始终存在
     rollingTime:0, 
 
-    resultPath:'d:/lottery/', //结果截图保存路径
+    resultPath:'/Users/86links/Pictures', //结果截图保存路径
     tasks:[
         //一个任务可以添加多抽奖活动，
         //每个抽奖活动消费一定数量的人员，
@@ -33,52 +33,31 @@ var config =  {
         //每个任务使用 users.txt中的名单
         {
             title:'开心百分百',
-            except:['胡歌','张铭恩'], //排除users.txt中的名单
+            except:[], //排除users.txt中的名单
             rewards:[
                 {
-                    title:'四等奖',  //奖项名称
-                    count:70,     //奖项数量
-                    capacity:35,//一次抽取数量[1，count]
-                    namesOfLine:6,  //获奖区每行显示几个名字 [1,2,3,4,6,12]
-                    nameFontSize: 24 //px
-                },
-                {
                     title:'三等奖', //奖项名称
-                    count:10,     //奖项数量
-                    capacity:5,//一次抽取数量[1，count]
-                    namesOfLine:3,  //获奖区每行显示几个名字 [1,2,3,4,6,12]
-                    nameFontSize: 32 //px
+                    count:6,     //奖项数量
+                    capacity:1,//一次抽取数量[1，count]
+                    namesOfLine:1,  //获奖区每行显示几个名字 [1,2,3,4,6,12]
+                    nameFontSize: 80 //px
                 },
                 {
                     title:'二等奖',  //奖项名称
-                    count:5,//奖项数量
-                    capacity:5,//一次抽取数量[1，count]
-                    namesOfLine:3,  //获奖区每行显示几个名字 [1,2,3,4,6,12]
-                    nameFontSize: 32 //px
+                    count:3,//奖项数量
+                    capacity:1,//一次抽取数量[1，count]
+                    namesOfLine:1,  //获奖区每行显示几个名字 [1,2,3,4,6,12]
+                    nameFontSize: 120 //px
                 },
                 {
                     title:'一等奖', //奖项名称
-                    count:5,     //奖项数量
-                    capacity:5,//一次抽取数量[1，count]
-                    namesOfLine:3,  //获奖区每行显示几个名字 [1,2,3,4,6,12]
-                    nameFontSize: 32 //px
+                    count:1,     //奖项数量
+                    capacity:1,//一次抽取数量[1，count]
+                    namesOfLine:1,  //获奖区每行显示几个名字 [1,2,3,4,6,12]
+                    nameFontSize: 160 //px
                 }
             ]
         },
-        
-        {
-            title:'幸运百分百',
-            except:['胡歌','李易峰'], //排除users.txt中的名单
-            rewards:[
-                {
-                    title:'幸运奖', //奖项名称
-                    count:1,     //奖项数量
-                    capacity:1,  //一次抽取数量[1，count]
-                    namesOfLine:1,  //获奖区每行显示几个名字 [1,2,3,4,6,12]
-                    nameFontSize: 64 //px
-                }
-            ]
-        }
     ]
 
 }
--- main.js
+++ main.js
@@ -21,33 +21,38 @@ const {
     })
   
     globalShortcut.unregisterAll();
+
+    // //开始抽奖
+    // globalShortcut.register('Return',()=>{
+    //   mainWindow.webContents.send('global-shortcut','toggle');
+    // });
   
-    //开始抽奖
-    globalShortcut.register('alt+s',()=>{
-      mainWindow.webContents.send('global-shortcut','start');
-    });
-    //停止抽奖
-    globalShortcut.register('alt+c',()=>{
-      mainWindow.webContents.send('global-shortcut','stop');
-    });
+    // //开始抽奖
+    // globalShortcut.register('CmdOrCtrl+s',()=>{
+    //   mainWindow.webContents.send('global-shortcut','start');
+    // });
+    // //停止抽奖
+    // globalShortcut.register('CmdOrCtrl+c',()=>{
+    //   mainWindow.webContents.send('global-shortcut','stop');
+    // });
     //切换到下一个奖项
-    globalShortcut.register('alt+n',()=>{
+    globalShortcut.register('CmdOrCtrl+n',()=>{
       mainWindow.webContents.send('global-shortcut','next');
     });
     //截屏
-    globalShortcut.register('alt+x',()=>{
+    globalShortcut.register('CmdOrCtrl+x',()=>{
       mainWindow.webContents.send('global-shortcut','capture');
     });
     //关闭/打开背景音乐
-    globalShortcut.register('alt+v',()=>{
+    globalShortcut.register('CmdOrCtrl+v',()=>{
       mainWindow.webContents.send('global-shortcut','novoice');
     });
     //退出
-    globalShortcut.register('ctrl+q',()=>{
+    globalShortcut.register('CmdOrCtrl+q',()=>{
       app.quit();
     });
     //调试
-    globalShortcut.register('ctrl+i',function(){
+    globalShortcut.register('CmdOrCtrl+i',function(){
       mainWindow.webContents.openDevTools();
     });
   
